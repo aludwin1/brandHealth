92c72387d999e1fd8cc8443adaf5bc2be8bc95e7
Aaron Ludwin <aludwin1@binghamton.edu>
2018-09-21 11:49:40 -0400

    testing api tie in

--- app.js
+++ app.js
@@ -4,8 +4,35 @@ const port = process.env.PORT || 3000;
 const api_key = process.env.api_key;
 const app_id = process.env.app_id;
 
+const AylienNewsApi = require('aylien-news-api');
+const apiInstance = new AylienNewsApi.DefaultApi();
+
+// Configure API key authorization: app_id
+const app_id = apiInstance.apiClient.authentications[app_id];
+app_id.apiKey = process.env.api_key;
+
+// Configure API key authorization: app_key
+var app_key = apiInstance.apiClient.authentications[app_id];
+app_key.apiKey = process.env.app_id;
+
+var opts = {
+  title: 'trump',
+  sortBy: 'social_shares_count.facebook',
+  language: ['en'],
+  notLanguage: ['es', 'it'],
+  publishedAtStart: 'NOW-7DAYS',
+  publishedAtEnd: 'NOW',
+  entitiesBodyLinksDbpedia: [
+    'http://dbpedia.org/resource/Donald_Trump',
+    'http://dbpedia.org/resource/Hillary_Rodham_Clinton',
+  ],
+};
+
 app.get('/', function(req, res) {
-  res.send(JSON.stringify({ Hello: 'World' }));
+  apiInstance.listStories(opts, (err, data) => {
+    if (err) res.send('There was an error with your request');
+    res.json(data.stories);
+  });
 });
 
 app.listen(port);
