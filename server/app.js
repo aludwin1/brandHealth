15726d6fe0494cabbfd240e701520628e1174bd8
Aaron Ludwin <aludwin1@binghamton.edu>
2018-09-21 11:35:26 -0400

    testing heroku app

--- server/app.js
+++ server/app.js
@@ -1,4 +1,11 @@
 const request = require('request');
 const app = require('express')();
+const port = process.env.PORT || 3000;
+const api_key = process.env.api_key;
+const app_id = process.env.app_id;
 
-app.listen(3000);
+app.get('/', function(req, res) {
+  res.send(JSON.stringify({ Hello: 'World' }));
+});
+
+app.listen(port);
